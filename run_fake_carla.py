ef4cceec65912a144ca7dd192ca0ad2c2dbef039
Kenneth Mead <kennethmead784@gmail.com>
2025-10-01 13:18:16 +0000

    updates

--- run_fake_carla.py
+++ run_fake_carla.py
@@ -1,11 +1,14 @@
 
 import contract.mqtt.client
+from on_vehicle_app.sensor_loop import run_fake_carla_sensor_loop
+import multiprocessing
 import on_vehicle_app.subscribers
-import on_vehicle_app.publishers
 
 contract.mqtt.client.initialize_mqtt_client()
 on_vehicle_app.subscribers.start_listening_to_topics()
-on_vehicle_app.publishers.publish_should_monitor_event()
+
+sensor_process = multiprocessing.Process(target=run_fake_carla_sensor_loop)
+sensor_process.start()
 
 # this blocks forever
 contract.mqtt.client.start_mqtt_client()
