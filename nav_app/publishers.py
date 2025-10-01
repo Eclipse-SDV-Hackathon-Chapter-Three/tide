fd24720fcac471668325abdca1895f789fc202ef
Kenneth Mead <kennethmead784@gmail.com>
2025-10-01 09:24:55 +0000

    added example nav app code

--- nav_app/publishers.py
+++ nav_app/publishers.py
@@ -13,5 +13,6 @@ def publish_should_monitor_event():
     CLIENT.publish(Topics.VEHICLE_ADAS_ACTOR_SHOULD_MONITOR, json.dumps(should_monitor_payload))
 
 def publish_actor_event_created(payload: AdasActorEvent):
+    print("Publishing actor event created:", payload)
     CLIENT.publish(Topics.VEHICLE_ADAS_ACTOR_EVENT_CREATED, json.dumps(payload))
 
